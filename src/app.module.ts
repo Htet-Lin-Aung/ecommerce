d4f141e9a7ccf2d9cf4b41298085119b74a5cb59
Htet Lin Aung <htetlinaungtuinfo@gmail.com>
2023-12-07 21:02:51 +0630

    added: auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module'; 
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/store'), 
-    ProductModule, UserModule, 
+    ProductModule, UserModule, AuthModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
