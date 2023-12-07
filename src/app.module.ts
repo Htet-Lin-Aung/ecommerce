a2683f1f3cf21e9197ee1f6c25a22d5fa8c86d7f
Htet Lin Aung <htetlinaungtuinfo@gmail.com>
2023-12-07 20:56:20 +0630

    added: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module'; 
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/store'), 
-    ProductModule, 
+    ProductModule, UserModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
